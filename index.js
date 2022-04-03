5d14c6746878fe33a1c6954233554ce8c510eb56
kristenli <kyli2005@gmail.com>
2022-04-03 19:06:39 -0400

    commit tree f3b5deb731a219a5f4152470bccc26601e412f7f

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400) return("This one is on me!")
+  if(ride <2000) return("That will be twenty bucks.")
+  if(ride > 2000 && ride <= 2500) return("I will gladly take your thirty bucks.")
+  if(ride >2500) return("No can do.")
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(whereto){
+  return whereto === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(benice){
+  switch(benice){
+    case 'generous' :
+      return('Thank you so much.')
+    case 'not as generous' :
+      return('Thank you.')
+    default:
+      return('Bye.')
+  }
 }
